162842e1a07fa7af4985685697d0da569f23beaf
LAUREN MILLER <lomiller0913@gmail.com>
2025-02-27 14:59:44 -0500

    troubleshooting scraper parsing

--- scraper.rb
+++ scraper.rb
@@ -3,16 +3,25 @@ require "nokogiri"
 require 'csv'
 
 # downloading the target web page 
-response = HTTParty.get("https://www.scrapingcourse.com/ecommerce/")
+# response = HTTParty.get("https://www.scrapingcourse.com/ecommerce/")
+# response = HTTParty.get("https://www.alltrails.com/us/oregon")
+# response = HTTParty.get("https://www.alltrails.com/explore?mobileMap=true&b_br_lat=42.409100246994285&b_br_lng=-116.9855834652293&b_tl_lat=45.9410508860702&b_tl_lng=-124.74697653477108")
 
-# response = HTTParty.get("https://www.scrapingcourse.com/ecommerce/", { 
+# response = HTTParty.get("https://www.alltrails.com/explore?mobileMap=true&b_br_lat=42.409100246994285&b_br_lng=-116.9855834652293&b_tl_lat=45.9410508860702&b_tl_lng=-124.74697653477108", { 
 # 	headers: { 
 # 		"User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36" 
 # 	}, 
 # })
+response = HTTParty.get("https://www.alltrails.com/us/oregon", { 
+	headers: { 
+		"User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36" 
+	}, 
+})
+
 
 # parsing the HTML document returned by the server 
 document = Nokogiri::HTML(response.body)
+p document
 
 # defining a data structure to store the scraped data 
 Product = Struct.new(:url, :image, :name, :price)
